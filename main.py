fbf8a191127c4f4dab6c4e5e5c13281bb5a1ed59
Zav48 <vik34reg@yandex.ru>
2024-04-02 12:54:29 +0900

    width and height
    создал переменные ширины и высоты окна

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import pygame
 
 pygame.init() # initialize all imported pygame modules - инициализировать все импортированные модули pygame
 
+#создать ширину и высоту окна.
+SKREEN_WIDTH = 600
+SKREEN_HEIGHT = 800
+
 runing = True # boolean variable - логическая переменная - когда нужно будет заверщить игру переменную сделаю = folse
 while runing:
     pass # пока что ни чего не делает(пропуск) нужна, чтобы не выходила ошибка
 
 pygame.quit() # closes all pygame modules - закрывает все модули pygame. завершает игру, как только закроется цикл (folse)
 
+#создать ширину и высоту окна. это делается в строчке после pygame.init()
 
 
 
