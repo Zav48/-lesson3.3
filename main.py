f4ad738dda9db1919a1469c8e9e6b560005caab7
Zav48 <vik34reg@yandex.ru>
2024-04-02 12:02:37 +0900

    added base code
    добавил базовый код

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init() # initialize all imported pygame modules - инициализировать все импортированные модули pygame
+
+runing = True # boolean variable - логическая переменная - когда нужно будет заверщить игру переменную сделаю = folse
+while runing:
+    pass # пока что ни чего не делает(пропуск) нужна, чтобы не выходила ошибка
+
+pygame.quit() # closes all pygame modules - закрывает все модули pygame. завершает игру, как только закроется цикл (folse)
+
+
+
+
